89b0c3bf18ef065b46d5a1b31b0ec730fc9d2152
wenbo.bai <wenbo.bai@vtradex.net>
2018-12-28 23:13:07 +0800

    first

--- backstage-manager/backstage-web/src/main/java/cn/vo/controller/hans/HansController.java
+++ backstage-manager/backstage-web/src/main/java/cn/vo/controller/hans/HansController.java
@@ -1,32 +1,25 @@
-package cn.vo.controller;
-
-
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+package cn.vo.controller.hans;
 
 
 import cn.vo.backstage.Utils.ListResult;
 import cn.vo.backstage.Utils.PageUtils;
 import cn.vo.pojo.User;
 import cn.vo.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
-@RequestMapping("users")
-public class UserController {
+@RequestMapping("hans")
+public class HansController {
 	
 	@Autowired
 	private IUserService iUserService;
@@ -43,7 +36,7 @@ public class UserController {
 	
 	@GetMapping("/list")
 	public String list(){
-		return "views/user/userList";
+		return "views/hans/reload";
 	}
 	@GetMapping("/listJson")
 	@ResponseBody
