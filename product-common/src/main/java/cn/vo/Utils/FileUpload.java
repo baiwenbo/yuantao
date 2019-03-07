8b145029ee5c77e457b80551aca98f766ac4a11a
ytjh <413143906@qq.com>
2019-03-08 01:11:12 +0800

    sss

--- product-common/src/main/java/cn/vo/Utils/FileUpload.java
+++ product-common/src/main/java/cn/vo/Utils/FileUpload.java
@@ -1,13 +1,35 @@
 package cn.vo.Utils;
 
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.IOUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class FileUpload {
+
+    //上传文件 到指定目录
+    public static String uploadFile(MultipartFile fileurl, String fileName) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String path = "";
+        try {
+            OutputStream out = null;
+            File newFile = new File("G:\\cs\\"
+                    + sdf.format(new Date()) + "\\");
+            if (!newFile.exists()) {
+                newFile.mkdirs();
+            }
+            path = "G:\\cs\\" + sdf.format(new Date()) + "\\"
+                    + new Date().getTime() + "_"
+                    + ((int) (Math.random() * 1000) + 1) + "_" + fileName;
+            File file2 = new File(path);
+            fileurl.transferTo(file2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return path;
+    }
 
 
 }
