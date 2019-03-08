70f03833f9f6662b251fd70763937b86a4b9f460
wenbo.bai <wenbo.bai@vtradex.net>
2019-03-08 20:08:20 +0800

    1

--- backstage-manager/backstage-dao/src/main/java/cn/vo/dao/hans/CustomerMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.vo.dao.hans;
-
-import cn.vo.pojo.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerMapper extends JpaRepository<Customer, Integer> {
-}
