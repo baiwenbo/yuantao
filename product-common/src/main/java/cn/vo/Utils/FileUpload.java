37073e8fdb0e13c161dd8db072c5207f24653b33
ytjh <413143906@qq.com>
2019-03-10 01:09:35 +0800

    gx

--- product-common/src/main/java/cn/vo/Utils/FileUpload.java
+++ product-common/src/main/java/cn/vo/Utils/FileUpload.java
@@ -12,23 +12,24 @@ public class FileUpload {
     //上传文件 到指定目录
     public static String uploadFile(MultipartFile fileurl, String fileName) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(fileName);
         String path = "";
+        if (fileurl.isEmpty()){
+            return  path;
+        }
         try {
             OutputStream out = null;
-            File newFile = new File("G:\\cs\\"
-                    + sdf.format(new Date()) + "\\");
+            File newFile = new File("G:\\cs\\");
             if (!newFile.exists()) {
                 newFile.mkdirs();
             }
-            path = "G:\\cs\\" + sdf.format(new Date()) + "\\"
-                    + new Date().getTime() + "_"
-                    + ((int) (Math.random() * 1000) + 1) + "_" + fileName;
+            path = "G:\\cs\\"  + fileurl.getOriginalFilename();
             File file2 = new File(path);
             fileurl.transferTo(file2);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return path;
+        return fileurl.getOriginalFilename();
     }
 
 
