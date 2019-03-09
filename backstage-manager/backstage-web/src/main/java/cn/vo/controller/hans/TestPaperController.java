d13632077f01a7d08c64c2cbd83266c69a7f05e6
ytjh <413143906@qq.com>
2019-03-09 18:07:41 +0800

    gx

--- backstage-manager/backstage-web/src/main/java/cn/vo/controller/hans/TestPaperController.java
+++ backstage-manager/backstage-web/src/main/java/cn/vo/controller/hans/TestPaperController.java
@@ -3,7 +3,6 @@ package cn.vo.controller.hans;
 
 import cn.vo.backstage.Utils.ListResult;
 import cn.vo.backstage.Utils.PageUtils;
-import cn.vo.dao.hans.TestPaperMapper;
 import cn.vo.pojo.Question;
 import cn.vo.pojo.User;
 import cn.vo.pojo.entity.TestPaper;
@@ -12,13 +11,14 @@ import cn.vo.service.IQuestionService;
 import cn.vo.service.ITestPaperService;
 import cn.vo.service.IUserService;
 import cn.vo.service.IXiaodianAddressService;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -43,14 +43,14 @@ public class TestPaperController {
 
 
     @GetMapping("list")
-    public  String  list(String close,Model model){
+    public  String  list(String close, Model model){
         model.addAttribute("close",close);
         return "views/hans/testPaperList";
     }
 
     @GetMapping("/listJson")
     @ResponseBody
-    public ListResult<TestPaper> listJson(HttpServletRequest request,String name, Integer page, Integer limit){
+    public ListResult<TestPaper> listJson(HttpServletRequest request, String name, Integer page, Integer limit){
         Map map=new HashMap<>();
         map.put("name", name);
         map.put("index", PageUtils.getPageIndex(page, limit));
@@ -102,22 +102,15 @@ public class TestPaperController {
 
     @GetMapping("edit")
     public String edit(Integer id, Model model){
-        List<Question> list=questionService.getType(1);
-        model.addAttribute("list",list);
+
         TestPaper testPaper=testPaperService.getById(id);
-        XiaodianAddress xiaodianAddress=xiaodianAddressService.getById(Long.valueOf(testPaper.getCompanyId()));
         model.addAttribute("testPaper",testPaper);
-        model.addAttribute("name",xiaodianAddress.getName());
         return "views/hans/questionetor";
     }
     @GetMapping("deial")
     public String deial(Integer id, Model model){
-        List<Question> list=questionService.getType(1);
-        model.addAttribute("list",list);
         TestPaper testPaper=testPaperService.getById(id);
-        XiaodianAddress xiaodianAddress=xiaodianAddressService.getById(Long.valueOf(testPaper.getCompanyId()));
         model.addAttribute("testPaper",testPaper);
-        model.addAttribute("name",xiaodianAddress.getName());
         return "views/hans/questionetor";
     }
 
