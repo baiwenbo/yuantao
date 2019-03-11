0c50546530acfa7c05129e7741f025e3e4694f89
yst <463861615@qq.com>
2019-03-11 11:23:13 +0800

    fenshuxiugai

--- product-common/src/main/java/cn/vo/Utils/FileUpload.java
+++ product-common/src/main/java/cn/vo/Utils/FileUpload.java
@@ -13,23 +13,27 @@ public class FileUpload {
     public static String uploadFile(MultipartFile fileurl, String fileName) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         System.out.println(fileName);
+        String resultUrl="";
         String path = "";
         if (fileurl.isEmpty()){
             return  path;
         }
         try {
+            Date date=new Date();
             OutputStream out = null;
-            File newFile = new File("G:\\cs\\");
+            File newFile = new File("E:\\images\\"+sdf.format(date)+"\\");
             if (!newFile.exists()) {
                 newFile.mkdirs();
             }
-            path = "G:\\cs\\"  + fileurl.getOriginalFilename();
+            String pathUrl=sdf.format(date)+"\\"+date.getTime()+fileurl.getOriginalFilename();
+             resultUrl=sdf.format(date)+"/"+date.getTime()+fileurl.getOriginalFilename();
+            path = "E:\\images\\"+pathUrl;
             File file2 = new File(path);
             fileurl.transferTo(file2);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return fileurl.getOriginalFilename();
+        return resultUrl;
     }
 
 
