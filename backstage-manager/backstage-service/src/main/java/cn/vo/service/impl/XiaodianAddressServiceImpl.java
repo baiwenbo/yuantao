297a2a180ee075a296e1be47c0c4c20b156e829f
ytjh <413143906@qq.com>
2019-03-15 22:27:56 +0800

    ss

--- backstage-manager/backstage-service/src/main/java/cn/vo/service/impl/XiaodianAddressServiceImpl.java
+++ backstage-manager/backstage-service/src/main/java/cn/vo/service/impl/XiaodianAddressServiceImpl.java
@@ -3,6 +3,7 @@ package cn.vo.service.impl;
 import cn.vo.dao.XiaodianAddressMapper;
 import cn.vo.dao.hans.XiaoDianAddressMapper;
 import cn.vo.pojo.entity.XiaodianAddress;
+import cn.vo.pojo.entity.XiaodianAddressExample;
 import cn.vo.service.IXiaodianAddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,5 +24,17 @@ public class XiaodianAddressServiceImpl implements IXiaodianAddressService {
     @Override
     public XiaodianAddress getById(Long id) {
         return xiaodianAddressMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public XiaodianAddress getCode(String code) {
+        XiaodianAddressExample xiaodianAddressExample=new XiaodianAddressExample();
+        XiaodianAddressExample.Criteria criteria=xiaodianAddressExample.createCriteria();
+        criteria.andCodeEqualTo(code);
+        List<XiaodianAddress> list=xiaodianAddressMapper.selectByExample(xiaodianAddressExample);
+        if (list.size()>0){
+            return list.get(0);
+        }
+        return null;
     }
 }
