63236f51cf97203f9f2ba21ea474de8f2aa3b0ef
ytjh <413143906@qq.com>
2019-03-21 22:29:58 +0800

    修改

--- backstage-manager/backstage-web/src/main/resources/static/layuiadmin/editDeial.js
+++ backstage-manager/backstage-web/src/main/resources/static/layuiadmin/editDeial.js
@@ -1,253 +1,253 @@
-$(document).ready(function(){
-    var th1=$("input[name='topic1One']:checked").val();
-    var th2=$("input[name='topic2One']:checked").val();
-    var th3=$("input[name='topic3One']:checked").val();
-    var th4=$("input[name='topic4One']:checked").val();
-    var th5=$("input[name='topic5One']:checked").val();
-    var th6=$("input[name='topic6One']:checked").val();
-    var th7=$("input[name='topic7One']:checked").val();
-    var th8=$("input[name='topic8One']:checked").val();
-    var th9=$("input[name='topic9One']:checked").val();
-    var th9t=$("input[name='topic9Two']:checked").val();
-    var th10=$("input[name='topic10One']:checked").val();
-    var th11=$("input[name='topic11One']:checked").val();
-    var th12=$("input[name='topic12One']:checked").val();
-    var th13=$("input[name='topic13One']:checked").val();
-    var th14=$("input[name='topic14One']:checked").val();
-    var th15=$("input[name='topic15One']:checked").val();
-    var th16=$("input[name='topic16One']:checked").val();
-    var th17=$("input[name='topic17One']:checked").val();
-    var th18=$("input[name='topic18One']:checked").val();
-    var th19=$("input[name='topic19One']:checked").val();
-    var th20=$("input[name='topic20One']:checked").val();
-    var th21=$("input[name='topic21One']:checked").val();
-    var th22=$("input[name='topic22One']:checked").val();
-    var th23=$("input[name='topic23One']:checked").val();
-    var th24=$("input[name='topic24One']:checked").val();
-    var th25=$("input[name='topic25One']:checked").val();
-    var th26=$("input[name='topic26One']:checked").val();
-    var th27=$("input[name='topic27One']:checked").val();
-    var th28=$("input[name='topic28One']:checked").val();
-    var th29=$("input[name='topic29One']:checked").val();
-    var th30=$("input[name='topic30One']:checked").val();
-    var th31=$("input[name='topic31One']:checked").val();
-    var th32=$("input[name='topic32One']:checked").val();
-    var th33=$("input[name='topic33One']:checked").val();
-    var th34=$("input[name='topic34One']:checked").val();
-    var th35=$("input[name='topic35One']:checked").val();
-    var th36=$("input[name='topic36One']:checked").val();
-    var th37=$("input[name='topic37One']:checked").val();
-    var th38=$("input[name='topic38One']:checked").val();
-    var th39=$("input[name='topic39One']:checked").val();
-    var th40=$("input[name='topic40One']:checked").val();
-if (th1.indexOf("完全")>0){
-    $("#one").hide();
-}else{
-    type1="2";
-}
-if (th2.indexOf("完全")>0){
-    $("#two").hide();
-}else{
-    type2="2";
-}
-if (th3.indexOf("完全")>0){
-    $("#three").hide();
-}else{
-    type3="2";
-}
-if (th4.indexOf("完全")>0){
-    $("#four").hide();
-}else{
-    type4="2";
-}
-if (th5.indexOf("完全")>0){
-    $("#five").hide();
-}else{
-    type5="2";
-}
-if (th6.indexOf("完全")>0){
-    $("#six").hide();
-}else{
-    type6="2";
-}
-if (th7.indexOf("完全")>0){
-    $("#seven").hide();
-}else{
-    type7="2";
-}
-if (th8.indexOf("完全")>0){
-    $("#eight").hide();
-}else{
-    type8="2";
-}
+$(document).ready(function () {
+    var th1 = $("input[name='topic1One']:checked").val();
+    var th2 = $("input[name='topic2One']:checked").val();
+    var th3 = $("input[name='topic3One']:checked").val();
+    var th4 = $("input[name='topic4One']:checked").val();
+    var th5 = $("input[name='topic5One']:checked").val();
+    var th6 = $("input[name='topic6One']:checked").val();
+    var th7 = $("input[name='topic7One']:checked").val();
+    var th8 = $("input[name='topic8One']:checked").val();
+    var th9 = $("input[name='topic9One']:checked").val();
+    var th9t = $("input[name='topic9Two']:checked").val();
+    var th10 = $("input[name='topic10One']:checked").val();
+    var th11 = $("input[name='topic11One']:checked").val();
+    var th12 = $("input[name='topic12One']:checked").val();
+    var th13 = $("input[name='topic13One']:checked").val();
+    var th14 = $("input[name='topic14One']:checked").val();
+    var th15 = $("input[name='topic15One']:checked").val();
+    var th16 = $("input[name='topic16One']:checked").val();
+    var th17 = $("input[name='topic17One']:checked").val();
+    var th18 = $("input[name='topic18One']:checked").val();
+    var th19 = $("input[name='topic19One']:checked").val();
+    var th20 = $("input[name='topic20One']:checked").val();
+    var th21 = $("input[name='topic21One']:checked").val();
+    var th22 = $("input[name='topic22One']:checked").val();
+    var th23 = $("input[name='topic23One']:checked").val();
+    var th24 = $("input[name='topic24One']:checked").val();
+    var th25 = $("input[name='topic25One']:checked").val();
+    var th26 = $("input[name='topic26One']:checked").val();
+    var th27 = $("input[name='topic27One']:checked").val();
+    var th28 = $("input[name='topic28One']:checked").val();
+    var th29 = $("input[name='topic29One']:checked").val();
+    var th30 = $("input[name='topic30One']:checked").val();
+    var th31 = $("input[name='topic31One']:checked").val();
+    var th32 = $("input[name='topic32One']:checked").val();
+    var th33 = $("input[name='topic33One']:checked").val();
+    var th34 = $("input[name='topic34One']:checked").val();
+    var th35 = $("input[name='topic35One']:checked").val();
+    var th36 = $("input[name='topic36One']:checked").val();
+    var th37 = $("input[name='topic37One']:checked").val();
+    var th38 = $("input[name='topic38One']:checked").val();
+    var th39 = $("input[name='topic39One']:checked").val();
+    var th40 = $("input[name='topic40One']:checked").val();
+    if (th1.indexOf("完全") > 0) {
+        $("#one").hide();
+    } else {
+        type1 = "2";
+    }
+    if (th2.indexOf("完全") > 0) {
+        $("#two").hide();
+    } else {
+        type2 = "2";
+    }
+    if (th3.indexOf("完全") > 0) {
+        $("#three").hide();
+    } else {
+        type3 = "2";
+    }
+    if (th4.indexOf("完全") > 0) {
+        $("#four").hide();
+    } else {
+        type4 = "2";
+    }
+    if (th5.indexOf("完全") > 0) {
+        $("#five").hide();
+    } else {
+        type5 = "2";
+    }
+    if (th6.indexOf("完全") > 0) {
+        $("#six").hide();
+    } else {
+        type6 = "2";
+    }
+    if (th7.indexOf("完全") > 0) {
+        $("#seven").hide();
+    } else {
+        type7 = "2";
+    }
+    if (th8.indexOf("完全") > 0) {
+        $("#eight").hide();
+    } else {
+        type8 = "2";
+    }
 
-if (th9.indexOf("无休闲区")>0){
-    $("#ninesuper").hide();
-}
-if (th9t.indexOf("完全")>0){
-    $("#nine").hide();
-}else{
-    type9="2";
-}
-if (th10.indexOf("完全")>0){
-    $("#ten").hide();
-}else{
-    type10="2";
-}
-if (th11.indexOf("完全")>0){
-    $("#eleven").hide();
-}else{
-    type11="2";
-}
-if (th12.indexOf("完全")>0){
-    $("#twelve").hide();
-}else{
-    type12="2";
-}
-if (th13.indexOf("完全")>0){
-    $("#thirteen").hide();
-}else{
-    type13="2";
-}
-if (th14.indexOf("完全")>0){
-    $("#th14").hide();
-}else{
-    type14="2";
-}
-if (th15.indexOf("完全")>0){
-    $("#th15").hide();
-}else{
-    type15="2";
-}
-if (th16.indexOf("完全")>0){
-    $("#th16").hide();
-}else{
-    type16="2";
-}
-    if (th17.indexOf("完全")>0){
+    if (th9.indexOf("无休闲区") > 0) {
+        $("#ninesuper").hide();
+    }
+    if (th9t.indexOf("完全") > 0) {
+        $("#nine").hide();
+    } else {
+        type9 = "2";
+    }
+    if (th10.indexOf("完全") > 0) {
+        $("#ten").hide();
+    } else {
+        type10 = "2";
+    }
+    if (th11.indexOf("完全") > 0) {
+        $("#eleven").hide();
+    } else {
+        type11 = "2";
+    }
+    if (th12.indexOf("完全") > 0) {
+        $("#twelve").hide();
+    } else {
+        type12 = "2";
+    }
+    if (th13.indexOf("完全") > 0) {
+        $("#thirteen").hide();
+    } else {
+        type13 = "2";
+    }
+    if (th14.indexOf("完全") > 0) {
+        $("#th14").hide();
+    } else {
+        type14 = "2";
+    }
+    if (th15.indexOf("完全") > 0) {
+        $("#th15").hide();
+    } else {
+        type15 = "2";
+    }
+    if (th16.indexOf("完全") > 0) {
+        $("#th16").hide();
+    } else {
+        type16 = "2";
+    }
+    if (th17.indexOf("完全") > 0) {
         $("#th17").hide();
-    }else{
-        type17="2";
+    } else {
+        type17 = "2";
     }
-    if (th18.indexOf("完全")>0){
+    if (th18.indexOf("完全") > 0) {
         $("#th18").hide();
-    }else{
-        type18="2";
+    } else {
+        type18 = "2";
     }
-    if (th19.indexOf("完全")>0){
+    if (th19.indexOf("完全") > 0) {
         $("#th19").hide();
-    }else{
-        type19="2";
+    } else {
+        type19 = "2";
     }
-    if (th20.indexOf("完全")>0){
+    if (th20.indexOf("完全") > 0) {
         $("#th20").hide();
-    }else{
-        type20="2";
+    } else {
+        type20 = "2";
     }
-    if (th21.indexOf("完全")>0){
+    if (th21.indexOf("完全") > 0) {
         $("#th21").hide();
-    }else{
-        type21="2";
+    } else {
+        type21 = "2";
     }
-    if (th22.indexOf("完全")>0){
+    if (th22.indexOf("完全") > 0) {
         $("#th22").hide();
-    }else{
-        type22="2";
+    } else {
+        type22 = "2";
     }
-    if (th23.indexOf("完全")>0){
+    if (th23.indexOf("完全") > 0) {
         $("#th23").hide();
-    }else{
-        type23="2";
+    } else {
+        type23 = "2";
     }
-    if (th24.indexOf("完全")>0){
+    if (th24.indexOf("完全") > 0) {
         $("#th24").hide();
-    }else{
-        type24="2";
+    } else {
+        type24 = "2";
     }
-    if (th25.indexOf("完全")>0){
+    if (th25.indexOf("完全") > 0) {
         $("#th25").hide();
-    }else{
-        type25="2";
+    } else {
+        type25 = "2";
     }
-    if (th26.indexOf("完全")>0){
+    if (th26.indexOf("完全") > 0) {
         $("#th26").hide();
-    }else{
-        type26="2";
+    } else {
+        type26 = "2";
     }
-    if (th27.indexOf("完全")>0){
+    if (th27.indexOf("完全") > 0) {
         $("#th27").hide();
-    }else{
-        type27="2";
+    } else {
+        type27 = "2";
     }
-    if (th28.indexOf("完全")>0){
+    if (th28.indexOf("完全") > 0) {
         $("#th28").hide();
-    }else{
-        type28="2";
+    } else {
+        type28 = "2";
     }
-    if (th29.indexOf("完全")>0){
+    if (th29.indexOf("完全") > 0) {
         $("#th29").hide();
-    }else{
-        type29="2";
+    } else {
+        type29 = "2";
     }
-    if (th30.indexOf("完全")>0){
+    if (th30.indexOf("完全") > 0) {
         $("#th30").hide();
-    }else{
-        type30="2";
+    } else {
+        type30 = "2";
     }
-    if (th31.indexOf("完全")>0){
+    if (th31.indexOf("完全") > 0) {
         $("#th31").hide();
-    }else{
-        type31="2";
+    } else {
+        type31 = "2";
     }
-    if (th32.indexOf("完全")>0){
+    if (th32.indexOf("完全") > 0) {
         $("#th32").hide();
-    }else{
-        type32="2";
+    } else {
+        type32 = "2";
     }
-    if (th33.indexOf("完全")>0){
+    if (th33.indexOf("完全") > 0) {
         $("#th33").hide();
-    }else{
-        type33="2";
+    } else {
+        type33 = "2";
     }
-    if (th34.indexOf("完全")>0){
+    if (th34.indexOf("完全") > 0) {
         $("#th34").hide();
-    }else{
-        type34="2";
+    } else {
+        type34 = "2";
     }
-    if (th35.indexOf("完全")>0){
+    if (th35.indexOf("完全") > 0) {
         $("#th35").hide();
-    }else{
-        type35="2";
+    } else {
+        type35 = "2";
     }
-    if (th36.indexOf("完全")>0){
+    if (th36.indexOf("完全") > 0) {
         $("#th36").hide();
-    }else{
-        type36="2";
+    } else {
+        type36 = "2";
     }
-    if (th37.indexOf("完全")>0){
+    if (th37.indexOf("完全") > 0) {
         $("#th37").hide();
-    }else{
-        type37="2";
+    } else {
+        type37 = "2";
     }
-    if (th38.indexOf("完全")>0){
+    if (th38.indexOf("完全") > 0) {
         $("#th38").hide();
-    }else{
-        type38="2";
+    } else {
+        type38 = "2";
     }
-    if (th39.indexOf("完全")>0){
+    if (th39.indexOf("完全") > 0) {
         $("#th39").hide();
-    }else{
-        type39="2";
+    } else {
+        type39 = "2";
     }
-    if (th40.indexOf("完全")>0){
+    if (th40.indexOf("完全") > 0) {
         $("#th40").hide();
-    }else{
-        type40="2";
+    } else {
+        type40 = "2";
     }
-    $("#queryCompany").click(function(){
+    $("#queryCompany").click(function () {
 
-        var code=$("#code").val();
-        if (code==""){
+        var code = $("#code").val();
+        if (code == "") {
             alert("门店编码不能为空");
             return;
         }
@@ -258,12 +258,12 @@ if (th16.indexOf("完全")>0){
         $("#company").val("");
         $("#companyId").val("");
         $.ajax({
-            type:"get",
-            url:path+"/environment/queryCode",
-            data: "code="+code,
-            dataType:"json",
-            async : false,
-            success:function(result){
+            type: "get",
+            url: path + "/environment/queryCode",
+            data: "code=" + code,
+            dataType: "json",
+            async: false,
+            success: function (result) {
                 $("#name").val(result.name);
                 $("#address").val(result.address);
                 $("#pianqu").val(result.pianqu);
@@ -271,7 +271,7 @@ if (th16.indexOf("完全")>0){
                 $("#company").val(result.company);
                 $("#companyId").val(result.id);
             },
-            error: function(result) {
+            error: function (result) {
                 alert("服务异常");
             }
 
@@ -280,7 +280,7 @@ if (th16.indexOf("完全")>0){
     });
 
 
-});
+})
 
 layui.config({
     base: path+'/layuiadmin/' //静态资源所在路径
